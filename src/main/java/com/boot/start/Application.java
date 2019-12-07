77460f91679bedff1f9bd8cce8ffc07b076fd5d8
jodyWeb <lhrocky@126.com>
2019-12-07 14:22:45 +0800

    new commit

--- src/main/java/com/boot/start/Application.java
+++ src/main/java/com/boot/start/Application.java
@@ -1,10 +1,8 @@
 package com.boot.start;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.boot"})
