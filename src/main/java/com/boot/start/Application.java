5319357a46155a5e2398dd5545a291790895f7ba
jodyWeb <lhrocky@126.com>
2019-12-07 14:11:09 +0800

    New Commit

--- src/main/java/com/boot/start/Application.java
+++ src/main/java/com/boot/start/Application.java
@@ -9,12 +9,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.boot"})
 public class Application {
-
     public static void main(String[] args) {
-
         SpringApplication.run( Application.class ,args );
-
     }
-
 }
 
