1a78cdab2f423d5f24b7f586985d05bfd46849ee
jodyWeb <lhrocky@126.com>
2019-12-07 13:52:55 +0800

    new

--- src/main/java/com/boot/model/JedisBean.java
+++ src/main/java/com/boot/model/JedisBean.java
@@ -1,13 +1,11 @@
 package com.boot.model;
 
-
-
 public class JedisBean {
 
     private String key;
     private String val;
 
-    public  JedisBean(String key,String val) {
+    public JedisBean(String key,String val) {
         this.key = key;
         this.val = val;
     }
